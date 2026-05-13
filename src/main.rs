a6bdf0308a5dd5d4272ef8e711ec2b45bf0b92ec
code_vibe <mrsamoladapo@gmail.com>
2026-05-13 07:10:45 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@
 use std::io::{self, Write};
 
 mod shell;
+
+
+struct  BuiltIn {
+     echo : String,
+     exit : String
+}
 fn main() {
      loop {
           print!("$ ");
@@ -14,7 +20,15 @@ fn main() {
                break;
           } else if command.starts_with("echo ") {
                println!("{}", &command[5..]);
-          } else {
+          } else if command.starts_with("type ") {
+               let output =  &command[5..];
+               match output {
+                    "echo" => println!("{}: is a shell builtin", output.to_string()),
+                    "exit" => println!("{}: is a shell builtin", output.to_string()),
+                    "type" => println!("{}: is a shell builtin", output.to_string()),
+                    _ => println!("{}: not found", output.to_string())
+               }
+          }else {
                println!("{}: command not found", command);
           }
      }
