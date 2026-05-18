59001a3bc7e7e8fd61254c914119faf64d538bd3
code_vibe <mrsamoladapo@gmail.com>
2026-05-18 04:43:36 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use pathsearch::find_executable_in_path;
 
 mod shell;
 
@@ -13,23 +14,48 @@ fn main() {
           print!("$ ");
           io::stdout().flush().unwrap();
           // Wait for user input
-          let mut command = String::new();
-          io::stdin().read_line(&mut command).unwrap();
-          command = command.trim().to_string();
-          if command == "exit" {
+
+          let mut input = "".to_string();
+          io::stdin().read_line(&mut input).unwrap(); //"type grep"
+
+          //input.pop();
+
+          let command: Vec<&str> = input.split(" ").collect();
+
+          if command[0] == "exit" {
                break;
-          } else if command.starts_with("echo ") {
-               println!("{}", &command[5..]);
-          } else if command.starts_with("type ") {
-               let output =  &command[5..];
-               match output {
-                    "echo" => println!("{} is a shell builtin", output.to_string()),
-                    "exit" => println!("{} is a shell builtin", output.to_string()),
-                    "type" => println!("{} is a shell builtin", output.to_string()),
-                    _ => println!("{}: not found", output.to_string())
-               }
+          }
+          eval_command(command[0], command[1..command.len()].to_vec());
+
+
+     }
+}
+
+
+fn eval_command(command: &str, args: Vec<&str>)  {
+     let known_commands = ["exit", "echo", "type"];
+
+     if !known_commands.contains(&command) {
+         println!("{}: command not found", command);
+          return;
+     }
+
+     if command == "echo" {
+          for arg in args {
+               println!("{}", arg);
+          }
+          println!();
+          return;
+     }
+
+     if command == "type" {
+          if known_commands.contains(&args[0]) {
+               println!("{} is a shell builtin", args[0]);
+          }else if let Some(path) = find_executable_in_path(&args[0]) {
+               println!("{} is {}", args[0], path.display());
           }else {
-               println!("{}: command not found", command);
+               println!("{}: not found", args[0]);
           }
      }
+
 }
